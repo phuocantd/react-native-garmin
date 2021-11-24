bc12d9b3dd1c722fbb566b5effd0aae44a25c46b
phuocantd <phuocantd@gmail.com>
2021-11-24 19:45:37 +0700

    config

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+export default App;
